c42f5cff485b1e8e7d0096a355b85fea5df55921
Artem Chivchalov <chivchalov@gmail.com>
2018-02-23 15:31:33 +0300

    refact: change `player-sandbox.js` example-mod

--- init_dist/example-mods/player-sandbox.js
+++ init_dist/example-mods/player-sandbox.js
@@ -1,15 +1,15 @@
 // Add a new function to player sandbox space
+// Try to run `test()` using your in-game console
 
 module.exports = function(config) {
     if(config.engine) {
-        config.engine.on('playerSandbox', function(sandbox) {
-
-            sandbox.Game.shard.name = 'my-private-server';
-
-            sandbox.test = function() {
-                sandbox.console.log('Current game tick is:', sandbox.Game.time);
-            }
-
+        config.engine.on('playerSandbox', function(sandbox, userId) {
+            sandbox.set('myInValue', 123);
+            sandbox.run(`function test() {
+                console.log('Current game tick is:', Game.time, 'myInValue:', global.myInValue);
+                global.myOutValue = 999; 
+            }`);
+            console.log(`User ${userId} global.myOutValue=${sandbox.get('myOutValue')}`);
         });
     }
 };
